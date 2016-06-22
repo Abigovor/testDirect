36dcc1f9f95f57bde8c248d35bca05ffd8b54fef
metalnikov.v <mv@lpsoft.ru>
2016-06-22 10:11:09 +0300

    Add default constructor in WelcomeMessage and construsctor with welcome message

--- src/src/WelcomeMessage.java
+++ src/src/WelcomeMessage.java
@@ -1,6 +1,14 @@
 
 public class WelcomeMessage {
-    private final String welcomeMsg = "Welcome";
+	private String welcomeMsg;
+
+	public WelcomeMessage(){
+		this.welcomeMsg = "Welcome";
+	}
+
+	public WelcomeMessage(String welcomMsg){
+		this.welcomeMsg = welcomMsg;
+	}
 
     public void sayHello( String userName ) {
     	System.out.println(welcomeMsg + " " + userName);
